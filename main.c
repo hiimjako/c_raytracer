30eeb4fddf7e42c7bc5e78a30f0374059ee68eed
Alberto Moretti <moretti919@gmail.com>
2025-08-15 23:59:41 +0200

    chapter 5

--- main.c
+++ main.c
@@ -3,11 +3,24 @@
 #include "vec3.h"
 #include <stdio.h>
 
+int hit_sphere(const point3 center, double radius, const ray r) {
+    vec3 oc = vec3_subtract(center , r.origin);
+    double a = vec3_dot(r.direction, r.direction);
+    double b = -2.0 * vec3_dot(r.direction, oc);
+    double c = vec3_dot(oc, oc) - radius*radius;
+    double discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 color3 ray_color(const ray r) {
+  if (hit_sphere(vec3_create(0,0,-1), 0.5, r) == 1)
+    return vec3_create(1, 0, 0);
+
   vec3 unit_direction = vec3_unit_vector(r.direction);
   double a = 0.5 * (vec3_y(unit_direction) + 1.0);
   double scaled_a = (1.0 - a);
 
+  // lerp interpolation function
   vec3 start_value = vec3_create(1.0, 1.0, 1.0);
   vec3 end_value = vec3_scale(vec3_create(0.5, 0.7, 1.0), a);
   vec3 blended_value = vec3_scale(start_value, scaled_a);
@@ -27,8 +40,7 @@ int main(void) {
   // Camera
   double focal_length = 1.0;
   double viewport_height = 2.0;
-  double viewport_width =
-      viewport_height * ((double)image_width / image_height);
+  double viewport_width = viewport_height * ((double)image_width / image_height);
   point3 camera_center = vec3_create_zero();
 
   // Calculate the vectors across the horizontal and down the vertical viewport
@@ -42,13 +54,19 @@ int main(void) {
 
   // Calculate the location of the upper left pixel.
   vec3 viewport_upper_left = vec3_subtract(
-      camera_center, vec3_subtract(vec3_create(0, 0, focal_length),
-                                   vec3_subtract(vec3_divide(viewport_u, 2),
-                                                 vec3_divide(viewport_v, 2))));
+            camera_center, 
+            vec3_add(
+                vec3_create(0, 0, focal_length),
+                vec3_add(
+                    vec3_divide(viewport_u, 2),
+                    vec3_divide(viewport_v, 2))
+                ));
 
-  vec3 pixel00_loc =
-      vec3_add(viewport_upper_left,
-               vec3_scale(vec3_add(pixel_delta_u, pixel_delta_v), 0.5));
+  vec3 pixel00_loc = vec3_add(
+          viewport_upper_left,
+          vec3_scale(
+              vec3_add(pixel_delta_u, pixel_delta_v), 
+              0.5));
 
   printf("P3\n%d %d\n255\n", image_width, image_height);
 
